c97fc4ccf39f41ac870ea531bd560d28bd064862
Halim-Al <halimwuw0@gmail.com>
2023-08-01 12:45:48 +0700

    test fixing

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ let navmbl = document.querySelector('.navmobile');
 let ullist = document.querySelector('.navbar')
 const hero = document.querySelector('.Hero')
 const imglayout = document.querySelector('.imglayout')
-const about = document.querySelector('.Abouttext')
+const abouttext = document.querySelector('.Abouttext')
+const about = document.querySelector('.About')
 const language = document.querySelector('.Language')
 const project = document.getElementById('Project')
 const myproject = document.querySelector('.myproject')
@@ -23,21 +24,25 @@ const observhero = new IntersectionObserver(entries =>{
             titlelayout.classList.toggle('show', entry.isIntersecting)
       
 
-        console.log(entry);
-       about.classList.toggle('show' , !entry.isIntersecting)
+       
+      
        imglayout.classList.toggle('show', entry.isIntersecting)
 
 
     })
-}
-,{
-    threshold: 0.1
-}
+} 
+
 )
 const observeskills = new IntersectionObserver(entries =>{
     entries.forEach(entry =>{
-        language.classList.toggle('show' , !entry.isIntersecting)
+        language.classList.toggle('show' , entry.intersectionRatio = 0.9)
+        abouttext.classList.toggle('show' , entry.isIntersecting)
+        abouttext.firstElementChild.classList.toggle('line', entry.isIntersecting)
+        console.log(entry);
+
+        
     })
+
 })
 
 const observeproject = new IntersectionObserver(entries =>{
