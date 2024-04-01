b3e7e9cb35939a60176b5e3922a4945f359817bb
Halim-Al <halimwuw0@gmail.com>
2024-04-02 05:31:21 +0700

    remove form

--- mail.php
+++ mail.php
@@ -20,8 +20,8 @@ if(isset($_POST['submit'])){
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'halimwuw0@gmail.com';                     //SMTP username
-        $mail->Password   = 'dlmftzqgortsmtui';                               //SMTP password
+        $mail->Username   = 'example.com';                     //SMTP username
+        $mail->Password   = 'secret';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     
